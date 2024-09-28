78ea9f5efea48adfdb219eadfbd425d2787f4c29
Gilah <kilah99@gmail.com>
2024-09-28 15:16:36 +0900

    처음 작업함

---
+++ src/main/java/kr/ac/koreantech/devopt_step0/DevoptStep0Application.java
@@ -0,0 +1,12 @@
+package kr.ac.koreantech.devopt_step0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoptStep0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoptStep0Application.class, args);
+	}
+}
---
+++ src/test/java/kr/ac/koreantech/devopt_step0/DevoptStep0ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.koreantech.devopt_step0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoptStep0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
