52690ef0410b19bc16ae6f4f103ebd7f32d01e55
Gilah <kilah99@gmail.com>
2024-09-29 11:46:20 +0900

    Test 코드 작성 후 action 확인

--- src/test/java/kr/ac/koreantech/devopt_step0/DevoptStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreantech/devopt_step0/DevoptStep0ApplicationTests.java
@@ -1,5 +1,7 @@
 package kr.ac.koreantech.devopt_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,22 @@ class DevoptStep0ApplicationTests {
 	void contextLoads() {
 	}
 
+	// 두 수를 더하는 함수(테스트 코드임)
+	@ㅆㄷㄴㅅ
+	int add(int a, int b) {
+		return a + b;
+
+	}
+
+	@Test // 그래들 빌드할 때 에러가 나면 동작을 멈춤
+	void addTest1() {
+		int result = add(10, 20);
+		assertEquals(30, result);
+	}
+
+	@Test // 그래들 빌드할 때 에러가 나면 동작을 멈춤
+	void addTest2() {
+		int result = add(10, 20);
+		assertEquals(10, result);
+	}
 }
